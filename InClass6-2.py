559c6e5a29287629d821e3fbf0745a3787734d7e
laurensart <artoflaurens@gmail.com>
2023-12-04 15:57:02 +0100

    added funfilter

--- InClass6-2.py
+++ InClass6-2.py
@@ -1,5 +1,4 @@
 import pygame
-
 class Filter():
     def __init__(self, image_src):
         self.image_src = image_src
@@ -25,14 +24,24 @@ class GrayScaleFilter(Filter):
                 avg = r / 3 + g / 3 + b / 3  # calculate average
                 self.arr[i][j] = [avg, avg, avg]  # set rgb values to average
 
+class FunnyFilter(Filter):
+    def transform(self):
+        for i in range(len(self.arr)):
+            for j in range(len(self.arr[i])):
+                self.arr[i][j] = [255 - self.arr[i][j][0], 255 - self.arr[i][j][1], 255 - self.arr[i][j][2]]
+
+
 pygame.init()
 screen = pygame.display.set_mode((1200, 400))
 
-# Use the filters
 image_file = "data/test_cut.png"
 gray_filter = GrayScaleFilter(image_file)
+funny_filter = FunnyFilter(image_file)
 
-# Apply GrayScaleFilter
 gray_filter.load_image()
 gray_filter.transform()
 gray_filter.save_image("data/test_gray.png")
+
+funny_filter.load_image()
+funny_filter.transform()
+funny_filter.save_image("data/test_funny.png")
