e94962465e43d69a20cfb289973e44c70b1f5e21
laurensart <artoflaurens@gmail.com>
2023-12-04 15:37:34 +0100

    refactored with 1 FIlter

--- InClass6-2.py
+++ InClass6-2.py
@@ -1,28 +1,38 @@
 import pygame
 
-# initialize pygame and screen
+class Filter():
+    def __init__(self, image_src):
+        self.image_src = image_src
+        self.img = None
+        self.arr = None
+
+    def load_image(self):
+        self.img = pygame.image.load(self.image_src).convert_alpha()
+        self.arr = pygame.surfarray.array3d(self.img)
+
+    def save_image(self, save_path):
+        surface = pygame.surfarray.make_surface(self.arr)
+        pygame.image.save(surface, save_path)
+
+    def transformer(self):
+        pass
+
+class GrayScaleFilter(Filter):
+    def transform(self):
+        for i in range(0, 16):  # column number from 0 to 15
+            for j in range(0, 16):  # row number from 0 to 15
+                r, g, b = self.arr[i][j]  # get rgb values for the pixel
+                avg = r / 3 + g / 3 + b / 3  # calculate average
+                self.arr[i][j] = [avg, avg, avg]  # set rgb values to average
+
 pygame.init()
 screen = pygame.display.set_mode((1200, 400))
 
-# load the image and convert to numpy array
-img = pygame.image.load('data/test_cut.png').convert_alpha()
-arr = pygame.surfarray.array3d(img)
+# Use the filters
+image_file = "data/test_cut.png"
+gray_filter = GrayScaleFilter(image_file)
 
-# print the shape of the array
-print(arr.shape)
-
-# print couple of pixels to understand the structure
-print(arr[2][0])
-print(arr[3][0])
-print(arr[4][0])
-
-
-# convert to grayscale using for loops
-for i in range(0, 16): # column number from 0 to 15
-    for j in range(0, 16): # row number from 0 to 15
-        r, g, b = arr[i][j] # get rgb values for the pixel
-        avg = r/3  + g/3  + b/3 # calculate average
-        arr[i][j] = [avg, avg, avg] # set rgb values to average
-
-surface = pygame.surfarray.make_surface(arr) # convert array back to pygame surface
-pygame.image.save(surface, 'data/test_gray.png') # save the surface as png
+# Apply GrayScaleFilter
+gray_filter.load_image()
+gray_filter.transform()
+gray_filter.save_image("data/test_gray.png")
